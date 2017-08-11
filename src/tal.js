d8f0c5b8cbd342c9fe43670b4880081c72ac15bd
Amed Reyes <areyes@nerd.com>
2017-08-11 16:30:16 -0400

    Standard code formating

--- src/tal.js
+++ src/tal.js
@@ -1,215 +1,198 @@
 'use strict'
 
 const NotContainerImplementation = new Error('Invalid container implementation class')
-const InvalidOnSetPrefix         = new Error('Missing onset sign prefix')
-const InvalidTal                 = new Error('Invalid Tal')
+const InvalidOnSetPrefix = new Error('Missing onset sign prefix')
+const InvalidTal = new Error('Invalid Tal')
 
-/*Time and duration separator ASCII 21*/
+/* Time and duration separator ASCII 21 */
 const TIME_SEP = 21
-/*Annotation separator ASCII 20*/
+/* Annotation separator ASCII 20 */
 const ANN_SEP = 20
-/*Annotation en*/
+/* Annotation en */
 const ANN_ENCODING = 'utf8'
-/*End of tal mark ASCII 0*/
-const EOT     = '\x00'.charCodeAt(0)
+/* End of tal mark ASCII 0 */
+const EOT = '\x00'.charCodeAt(0)
 
-
-/*Internal TAL parsing states*/
+/* Internal TAL parsing states */
 const MINUS_SIGN = 45
-const PLUS_SIGN  = 43
-
-
+const PLUS_SIGN = 43
 
 var TalContainer = class {
+  constructor () {
+    this.offset = 0.0
+    this.duration = 0.0
+    this.ann = []
+  }
 
-    constructor(){
-        this.offset = 0.0
-        this.duration = 0.0
-        this.ann = []
-    }
+  OnSet (offset) {
+    this.offset = offset
+    return this
+  }
 
-    OnSet(offset){
-        this.offset = offset
-        return this
-    }
+  Duration (duration) {
+    this.duration = duration
+    return this
+  }
 
-    Duration(duration){
-        this.duration = duration
-        return this
-    }
-
-    AddAnnotation(msg){
-        this.ann.push(
+  AddAnnotation (msg) {
+    this.ann.push(
             msg.toString(ANN_ENCODING)
         )
-        return this
-    }
+    return this
+  }
 }
 
-var MultiTalContainer = class{
-
-    constructor(container){
-
-
-        if(!IsTalContainer(container.prototype)){
-            throw NotContainerImplementation
-        }
-
-        this.container = container
-        this.list    = []
-        this.current = null
+var MultiTalContainer = class {
+  constructor (container) {
+    if (!IsTalContainer(container.prototype)) {
+      throw NotContainerImplementation
     }
 
-    OnSet(offset){
+    this.container = container
+    this.list = []
+    this.current = null
+  }
 
-        this.current = new this.container
+  OnSet (offset) {
+    this.current = new this.container()
 
-        this.list.push(
+    this.list.push(
             this.current
         )
 
-        this.current.OnSet(offset)
-        return this;
-    }
+    this.current.OnSet(offset)
+    return this
+  }
 
-    Duration(duration){
-        this.current.Duration(duration)
-        return this
-    }
+  Duration (duration) {
+    this.current.Duration(duration)
+    return this
+  }
 
-    AddAnnotation(text){
-        this.current.AddAnnotation(text)
-        return this
-    }
+  AddAnnotation (text) {
+    this.current.AddAnnotation(text)
+    return this
+  }
 
-    Tals(){
-        return this.list
-    }
+  Tals () {
+    return this.list
+  }
 }
 
-
-function IsTalContainer(obj){
-    return (
+function IsTalContainer (obj) {
+  return (
         obj.OnSet && (obj.OnSet instanceof Function) &&
         obj.Duration && (obj.Duration instanceof Function) &&
         obj.AddAnnotation && (obj.AddAnnotation instanceof Function)
-    )
+  )
 }
-
 
 /*
     Given a source array and a container implementation
     decode the first TAL and return
 */
-function TalDecoder(source, container){
+function TalDecoder (source, container) {
+  var current = 0
+  const length = source.length
 
-    var current = 0
-    const length = source.length
+  while (source[current] === EOT && current < length) {
+    current++
+  }
 
-    while( source[current] == EOT && current < length){
-        current++
-    }
+  if (length === current) {
+    return current
+  }
 
-    if(length == current){
-        return current
-    }
+    // onset
+  const sign = source[ current++ ]
+  if (sign !== MINUS_SIGN && sign !== PLUS_SIGN) {
+    throw InvalidOnSetPrefix
+  }
 
-    //onset
-    const sign = source[ current++ ]
-    if(sign != MINUS_SIGN && sign != PLUS_SIGN){
-        throw InvalidOnSetPrefix
-    }
+  var next = nextToken(source, current)
 
-    var next = nextToken(source, current)
+  if (next === false || source[next] === EOT) {
+    throw InvalidTal
+  }
 
-    if(false == next || source[next] == EOT){
-        throw InvalidTal
-    }
-
-    var offset = parseFloat(
-        source.toString('ascii',current, next)
+  var offset = parseFloat(
+        source.toString('ascii', current, next)
     )
 
-    if(isNaN(offset)){
-        throw InvalidTal
+  if (isNaN(offset)) {
+    throw InvalidTal
+  }
+
+  if (sign === MINUS_SIGN) {
+    offset *= -1
+  }
+  current = next
+
+    // duration
+  var duration = null
+  if (source[current++] === TIME_SEP) {
+    next = nextToken(source, current)
+    if (next === false || source[next] !== ANN_SEP) {
+      throw InvalidTal
     }
 
-    if(sign == MINUS_SIGN){
-        offset *= -1
-    }
-    current = next
-
-    //duration
-    var duration = null
-    if(source[current++] == TIME_SEP){
-
-        next = nextToken(source, current)
-        if(false == next || source[next] != ANN_SEP){
-            throw InvalidTal
-        }
-
-        duration = parseFloat(
-            source.toString('ascii',current, next)
+    duration = parseFloat(
+            source.toString('ascii', current, next)
         )
 
-        if(isNaN(duration)){
-            throw InvalidTal
-        }
-
-        current = next + 1
+    if (isNaN(duration)) {
+      throw InvalidTal
     }
 
-    container.OnSet(offset)
-    container.Duration(duration)
+    current = next + 1
+  }
 
-    //annotations
-    while (next = nextToken(source, current)){
+  container.OnSet(offset)
+  container.Duration(duration)
 
-        if (source[next] != ANN_SEP){
-            break
-        }
+    // annotations
+  while (next = nextToken(source, current)) {
+    if (source[next] !== ANN_SEP) {
+      break
+    }
 
-        container.AddAnnotation(
+    container.AddAnnotation(
             source.slice(current, next)
         )
 
-        current = next+1
-    }
+    current = next + 1
+  }
 
-    if(next == false || source[next] != EOT){
-        throw InvalidTal
-    }
+  if (next === false || source[next] !== EOT) {
+    throw InvalidTal
+  }
 
-    return next + 1
-
+  return next + 1
 }
 
+function nextToken (source, start) {
+  var pos = start
+  const len = source.length
+  while (pos < len) {
+    const at = source[pos]
 
-function nextToken(source, start){
-
-    var pos = start
-    const len = source.length
-    while(pos < len){
-
-        const at = source[pos]
-
-        if(
-            TIME_SEP == at ||
-            ANN_SEP  == at ||
-            EOT      == at
-        ){
-            return pos
-        }
-
-        pos++
+    if (
+            TIME_SEP === at ||
+            ANN_SEP === at ||
+            EOT === at
+        ) {
+      return pos
     }
 
-    return false
+    pos++
+  }
+
+  return false
 }
 
 module.exports = {
-    IsTalContainer,
-    TalDecoder,
-    MultiTalContainer,
-    TalContainer
+  IsTalContainer,
+  TalDecoder,
+  MultiTalContainer,
+  TalContainer
 }
--- tests/tal.js
+++ tests/tal.js
@@ -1,178 +1,152 @@
 'use strict'
 
 const assert = require('assert')
-const tal    = require('../src/tal.js')
+const tal = require('../src/tal.js')
 
+describe('TalContainerDefinition', function () {
+  it('IsTalContainer', function () {
+    var mock = {
+      OnSet: (onset) => {
 
-describe("TalContainerDefinition", function(){
+      },
+      Duration: (duration) => {
 
-    it("IsTalContainer", function(){
+      },
+      AddAnnotation: (msg) => {
 
-        var mock = {
-            OnSet: (onset) => {
+      }
+    }
 
-            },
-            Duration: (duration) => {
-
-            },
-            AddAnnotation: (msg) => {
-
-            },
-        }
-
-        assert.equal(true, tal.IsTalContainer(mock),"TalContainer Interface")
-
-    })
+    assert.equal(true, tal.IsTalContainer(mock), 'TalContainer Interface')
+  })
 })
 
+describe('TalContainer', function () {
+  it('IsTalContainer', function () {
+    const ctner = new tal.TalContainer()
 
-describe("TalContainer", function(){
+    assert.equal(true, tal.IsTalContainer(ctner), 'TalContainer implements the interface')
+  })
 
-    it("IsTalContainer", function(){
-        const ctner = new tal.TalContainer()
+  it('OnSet', function () {
+    var ctner = new tal.TalContainer()
+    const onset = 1.1
 
-        assert.equal(true, tal.IsTalContainer(ctner),"TalContainer implements the interface")
-    })
+    assert.equal(ctner.offset, 0.0, 'Default tal onset')
+    assert.equal(ctner.OnSet(onset), ctner, 'Method Chain')
+    assert.equal(ctner.offset, onset, 'Set onset')
+  })
 
-    it("OnSet", function(){
+  it('Duration', function () {
+    var ctner = new tal.TalContainer()
+    const duration = 1.1
 
-        var ctner = new tal.TalContainer()
-        const onset = 1.1
+    assert.equal(ctner.duration, 0.0, 'Default tal duration')
+    assert.equal(ctner.Duration(duration), ctner, 'Method Chain')
+    assert.equal(ctner.duration, duration, 'Set duration')
+  })
 
-        assert.equal(ctner.offset,0.0, "Default tal onset")
-        assert.equal(ctner.OnSet(onset),ctner,"Method Chain")
-        assert.equal(ctner.offset,onset ,"Set onset")
+  it('Annotations', function () {
+    var ctner = new tal.TalContainer()
+    const annotations = ['ann1', 'ann2']
 
-    })
-
-
-    it("Duration", function(){
-
-        var ctner = new tal.TalContainer()
-        const duration = 1.1
-
-        assert.equal(ctner.duration,0.0,"Default tal duration")
-        assert.equal(ctner.Duration(duration),ctner ,"Method Chain")
-        assert.equal(ctner.duration,duration,"Set duration")
-
-    })
-
-    it("Annotations", function(){
-
-        var ctner = new tal.TalContainer()
-        const annotations = ["ann1","ann2"]
-
-        assert.deepStrictEqual(ctner.ann,[],"Default Empty list")
-        assert.equal(ctner.AddAnnotation(annotations[0]),ctner,"MethodChain")
-        ctner.AddAnnotation(annotations[1])
-        assert.deepStrictEqual(ctner.ann, annotations)
-    })
+    assert.deepStrictEqual(ctner.ann, [], 'Default Empty list')
+    assert.equal(ctner.AddAnnotation(annotations[0]), ctner, 'MethodChain')
+    ctner.AddAnnotation(annotations[1])
+    assert.deepStrictEqual(ctner.ann, annotations)
+  })
 })
 
-
-describe("MultiTalContainer", function(){
-
-    it("IsTalContainer", function(){
-        const ctner = new tal.MultiTalContainer(
+describe('MultiTalContainer', function () {
+  it('IsTalContainer', function () {
+    const ctner = new tal.MultiTalContainer(
             tal.TalContainer
         )
 
-        assert.equal(true, tal.IsTalContainer(ctner),"MultiTalContainer implements the interface")
-    })
+    assert.equal(true, tal.IsTalContainer(ctner), 'MultiTalContainer implements the interface')
+  })
 
+  it('Not container provided', function () {
+    assert.throws(() => {
+      return new tal.MultiTalContainer({})
+    }, Error, 'Invalid container implementation class')
+  })
 
-    it("Not container provided", function(){
-
-        assert.throws(() => {
-            return new tal.MultiTalContainer({})
-        }, Error, 'Invalid container implementation class')
-    })
-
-
-    it("Single Tal", function(){
-        const ctner = new tal.MultiTalContainer(
+  it('Single Tal', function () {
+    const ctner = new tal.MultiTalContainer(
             tal.TalContainer
         )
 
-        const offset = 1.0
-        const duration = 1.0
-        const msg  = "First Tal"
+    const offset = 1.0
+    const duration = 1.0
+    const msg = 'First Tal'
 
-        assert.equal(ctner.OnSet(offset),ctner, "Onset method chain")
-        assert.equal(ctner.Duration(duration), ctner, "Duration method chain")
-        assert.equal(ctner.AddAnnotation(msg), ctner, "Duration method chain")
+    assert.equal(ctner.OnSet(offset), ctner, 'Onset method chain')
+    assert.equal(ctner.Duration(duration), ctner, 'Duration method chain')
+    assert.equal(ctner.AddAnnotation(msg), ctner, 'Duration method chain')
 
-        const list = ctner.Tals()
+    const list = ctner.Tals()
 
+    assert.equal(list.length, 1, 'Single tal')
+    assert((list[0] instanceof tal.TalContainer), true, 'Correnct instance')
+    var expected = new tal.TalContainer()
 
-        assert.equal(list.length, 1,"Single tal")
-        assert((list[0] instanceof tal.TalContainer), true,"Correnct instance")
-        var expected = new tal.TalContainer()
-
-        assert.deepStrictEqual(
+    assert.deepStrictEqual(
             expected.OnSet(offset).Duration(duration).AddAnnotation(msg),
             list[0],
-            "Equal tal"
+            'Equal tal'
         )
-    })
+  })
 })
 
+describe('Tal Decoder', function () {
+  var tests = [
+        {input: '+660\x15300\x14Sleep stage N1\x14\x00', length: 25, offset: 660.0, duration: 300.0, annotations: ['Sleep stage N1']},
+        {input: '+0\x14\x14Recording starts\x14\x00', length: 22, offset: 0.0, duration: null, annotations: ['', 'Recording starts']},
+        {input: '+1019.4\x150.8\x14Limb movement\x14R leg\x14\x00', length: 33, offset: 1019.4, duration: 0.8, annotations: ['Limb movement', 'R leg']},
+        {input: '-302102\x14Recording ends\x14\x00\x00\x00\x00\x00', length: 24, offset: -302102, duration: null, annotations: ['Recording ends']}
+  ]
 
-describe("Tal Decoder",function(){
+  tests.forEach(function (test, index) {
+    it('Tal decoder test ' + test.input, function () {
+      var container = new tal.TalContainer()
+      const used = tal.TalDecoder(Buffer.from(test.input), container)
 
-    var tests = [
-        {input: "+660\x15300\x14Sleep stage N1\x14\x00", length:25, offset: 660.0, duration: 300.0, annotations: ["Sleep stage N1"]},
-        {input: "+0\x14\x14Recording starts\x14\x00", length:22, offset: 0.0, duration: null, annotations: ["","Recording starts"]},
-        {input: "+1019.4\x150.8\x14Limb movement\x14R leg\x14\x00", length:33, offset: 1019.4, duration: 0.8, annotations: ["Limb movement","R leg"]},
-        {input: "-302102\x14Recording ends\x14\x00\x00\x00\x00\x00", length:24, offset: -302102, duration: null, annotations: ["Recording ends"]}
-    ]
-
-    tests.forEach(function(test, index){
-        it("Tal decoder test " + test.input, function(){
-            var container = new tal.TalContainer()
-            const used = tal.TalDecoder(Buffer.from(test.input), container)
-
-            assert.equal(used, test.length,"Parsed amount of bytes")
-            assert.equal(container.offset, test.offset,"Tal offset")
-            assert.equal(container.duration, test.duration,"Tal duration")
-            assert.deepStrictEqual(container.ann, test.annotations,"Tal annotations")
-        })
+      assert.equal(used, test.length, 'Parsed amount of bytes')
+      assert.equal(container.offset, test.offset, 'Tal offset')
+      assert.equal(container.duration, test.duration, 'Tal duration')
+      assert.deepStrictEqual(container.ann, test.annotations, 'Tal annotations')
     })
+  })
 
+  it('Only 0s', function () {
+    var container = new tal.MultiTalContainer(tal.TalContainer)
+    const input = Buffer.from('\x00\x00\x00\x00\x00\x00')
 
-    it("Only 0s", function(){
+    const used = tal.TalDecoder(input, container)
+    assert.equal(used, 6)
+    assert.equal(0, container.Tals().length)
+  })
 
-        var container = new tal.MultiTalContainer(tal.TalContainer)
-        const input  = Buffer.from("\x00\x00\x00\x00\x00\x00")
+  var bad = [
+        {input: '*660\x15300\x14Sleep stage N1\x14\x00', error: 'Missing onset sign prefix'},
+        {input: '+660', error: 'Invalid Tal'},
+        {input: '+660\x00', error: 'Invalid Tal'},
+        {input: '+abc\x15', error: 'Invalid Tal'},
+        {input: '+123\x15\x00', error: 'Invalid Tal'},
+        {input: '+123\x15abc\x14', error: 'Invalid Tal'},
+        {input: '+123\x15123\x14', error: 'Invalid Tal'},
+        {input: '+123\x15123', error: 'Invalid Tal'}
+  ]
 
-        const used = tal.TalDecoder(input, container)
-        assert.equal(used,6)
-        assert.equal(0,container.Tals().length)
-    })
-
-    var bad_tals = [
-        {input: "*660\x15300\x14Sleep stage N1\x14\x00", error: "Missing onset sign prefix"},
-        {input: "+660", error: "Invalid Tal"},
-        {input: "+660\x00", error: "Invalid Tal"},
-        {input: "+abc\x15", error: "Invalid Tal"},
-        {input: "+123\x15\x00", error: "Invalid Tal"},
-        {input: "+123\x15abc\x14", error: "Invalid Tal"},
-        {input: "+123\x15123\x14", error: "Invalid Tal"},
-        {input: "+123\x15123", error: "Invalid Tal"},
-    ]
-
-    bad_tals.forEach(function(test, index){
-        it("Bad Tal decoder test " + test.input, function(){
-
-            assert.throws(
+  bad.forEach(function (test, index) {
+    it('Bad Tal decoder test ' + test.input, function () {
+      assert.throws(
                 () => {
-                    tal.TalDecoder(Buffer.from(test.input), new tal.TalContainer())
+                  tal.TalDecoder(Buffer.from(test.input), new tal.TalContainer())
                 },
                 Error, test.error
             )
-
-        })
     })
-
-
+  })
 })
